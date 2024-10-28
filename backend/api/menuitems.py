2ded4a628f0500ef3c1d8d08bf7d68decc1367de
dperry17 <darin.peries@gmail.com>
2024-10-28 15:51:19 -0500

    created POST api for menuitem

--- backend/api/menuitems.py
+++ backend/api/menuitems.py
@@ -18,3 +18,24 @@ def get_menuitems():
         return jsonify({"error": "could not get menu items"}), 500
     
 
+@menuitem_bp.route('/create', methods=['POST'])
+def create_menuitems():
+    data = request.json
+    name = data['name']
+    category = data['category']
+    price = data['price']
+    ingredients = data['ingredients'] #list
+
+    try:
+        with get_db_connection() as conn:
+            with conn.cursor(cursor_factory=RealDictCursor) as cur:
+                cur.execute("INSERT INTO menu_items (menu_item_name, category, price) VALUES (%s, %s, %s) RETURNING menu_item_id;", (name, category, price))
+                menu_item_id = cur.fetchone()
+                for ingredient in ingredients:
+                    ingredient_id = cur.execute("SELECT ingredient_id FROM ingredients WHERE ingredient_name = %s", ingredient)
+                    stock = cur.execute("SELECT stock WHERE ingredient_id = %s", (ingredient_id))
+                    cur.execute("INSERT INTO menu_items_ingredients (menu_item_id, ingredient_id, ingredient_amount) VALUES (%s, %s, %s);", (menu_item_id, ingredient_id, stock))
+        return jsonify(menu_item_id), 200
+    except psycopg2.Error as e:
+        print(f"Error creating menu item: {e}")
+        return jsonify({"error": "could not create menu item"}), 500
