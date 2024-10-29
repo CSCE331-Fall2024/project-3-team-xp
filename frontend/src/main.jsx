6ff2d648189474b79d70c62dc8c563c7535dd4ce
root <root@Kernel>
2024-10-29 13:41:49 -0500

    Menu Board complete and testing backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import ReportsView from './manager/ReportsView';
+import MenuBoard from './menuBoard/menuBoard';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/manager/ReportsView" element={<ReportsView />} />
+        <Route path="/menuBoard/MenuBoard" element={<MenuBoard />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
