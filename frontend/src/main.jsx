ea0ab4a13adbb2ca30424a6e8f9190983001495c
vedarth31 <vedarth31@gmail.com>
2024-10-29 16:42:31 -0500

    copied over navbar; fixed tailwind

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Employees from './manager/employees';
 import CashierPanel from './cashier/CashierView';
+import Navbar from './components/Navbar';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/manager/employees" element={<Employees />}/>
         <Route path="/cashier" element={<CashierPanel />}/>
