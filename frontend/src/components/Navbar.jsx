fb956207cebfa1258a2cad30654332210cd254f1
vedarth31 <vedarth31@gmail.com>
2024-11-11 13:00:30 -0600

    updated user states, protected routes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import PandaLogo from '../assets/PandaLogo.png'
 import { Link } from 'react-router-dom';
 import SelectLang from '../translation/translationWidget'
+import { useAuth } from '../lib/AuthContext';
 
 const Navbar = () => {
+  const { user } = useAuth();
+  const handleLogout = () => {
+    window.location.href = 'http://127.0.0.1:5000/logout';
+  }
+
   return (
     <nav className="top-0 left-0 w-full flex items-center justify-between bg-[#ffd5d5] px-[10px] py-[3px] z-[1000]">
       <div>
@@ -43,12 +49,22 @@ const Navbar = () => {
       </div>
 
       <div className="mr-[10px]">
-        <Link
-          to="/login"
-          className="inline-block px-[15px] py-[3px] text-base cursor-pointer bg-[#E53935] text-white rounded-[20px_1%] transition-all duration-300 hover:bg-[#F55A4E] hover:scale-110"
-        >
-          Log in
-        </Link>
+
+        {!user ? (
+          <Link
+            to="/login"
+            className="inline-block px-[15px] py-[3px] text-base cursor-pointer bg-[#E53935] text-white rounded-[20px_1%] transition-all duration-300 hover:bg-[#F55A4E] hover:scale-110"
+          >
+            Log in
+          </Link>
+        ) : (
+          <button
+            onClick={handleLogout}
+            className="inline-block px-[15px] py-[3px] text-base cursor-pointer bg-[#E53935] text-white rounded-[20px_1%] transition-all duration-300 hover:bg-[#F55A4E] hover:scale-110"
+          >
+            Log out
+          </button>
+        )}
       </div>
     </nav>
   );
