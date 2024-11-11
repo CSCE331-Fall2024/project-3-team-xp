cf06415d2bd3b1db0fb8fa474b533a2a1ed4662e
dperry17 <darin.peries@gmail.com>
2024-11-10 22:50:22 -0600

    added chunking functionality for large amounts of texts

--- frontend/src/translation/translate.jsx
+++ frontend/src/translation/translate.jsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from 'react';
 import getAllTextNodes from './extract';
 
 const translateText = async (texts, targetLang) =>{
+    console.log(texts);
     const API_KEY = import.meta.env.VITE_API_KEY; 
     const url = `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`; 
     const response = await fetch(url, {
@@ -23,8 +24,20 @@ function useTranslatePage(targetLang) {
     const getRootContentHash = () => (rootElement ? rootElement.innerHTML : '');
     useEffect(() =>{
         const translatePage = async () =>{
+            const maxChunkSize = 50;
+            let translatedTexts = [];
             const {textNodes, textsToTranslate} = getAllTextNodes(rootElement);
-            const translatedTexts = await translateText(textsToTranslate, targetLang);
+            if(textsToTranslate.length > maxChunkSize){
+                let chunk = [];
+                for (let i = 0; i < textsToTranslate.length; i += maxChunkSize) {
+                    chunk = textsToTranslate.slice(i, i + maxChunkSize);
+                    const translatedChunk = await translateText(chunk, targetLang);
+                    translatedTexts = translatedTexts.concat(translatedChunk);
+                }
+            } 
+            else{
+                translatedTexts = await translateText(textsToTranslate, targetLang);
+            }
             textNodes.forEach((node, index) => {
                 node.nodeValue = translatedTexts[index];
             });
