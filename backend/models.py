e9c879e0f399b26b7637ab67010832515ace720d
vedarth31 <vedarth31@gmail.com>
2024-11-22 11:55:57 -0600

    added current points to users, track points during orders

--- backend/models.py
+++ backend/models.py
@@ -8,8 +8,12 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     name = db.Column(db.String(50))
     account = db.Column(db.String(50))
+    current_points = db.Column(db.Integer)
+    total_points = db.Column(db.Integer)
 
-    def __init__(self, email, name, account=None):
+    def __init__(self, email, name, account=None, current_points=0, total_points=0):
         self.email = email
         self.name = name
         self.account = account
+        self.current_points = current_points
+        self.total_points = total_points
